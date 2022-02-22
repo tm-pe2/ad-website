ccf381c93f7f6fd53d2d64337e0f1607b574be75
DrunkenToast <peter5leconte@gmail.com>
2022-02-22 20:53:40 +0100

    Perm fix linux script

--- composer.sh
+++ composer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 CMD="composer bash"
-if [ $1 == "serve" ]; then CMD="composer php artisan serve"; fi
+if [ "$1" == "serve" ]; then CMD="composer php artisan serve"; fi
 
 sudo docker run --rm --interactive --tty --net=host \
   --volume ${PWD}:/app \
-  --user $(id -u):$(id -g) \
+  --user $(id -u ${USER}):$(id -g ${USER}) \
   $CMD
