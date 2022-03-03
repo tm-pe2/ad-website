e48dcf3d220c7761491462bc55305368975e2418
Coockie1173 <rudjerud@hotmail.com>
2022-03-03 10:32:47 +0100

    now can just feed array into component and it works rather than having to do every line one by one
    probably could've done this on the HTML side of things but whatever, it works

--- src/app/technician-todo/techd-table-row/techd-table-row.component.ts
+++ src/app/technician-todo/techd-table-row/techd-table-row.component.ts
@@ -1,18 +1,39 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-techd-table-row',
+  selector: 'app-techd-table', //I know the filename has row but it's actually a full on table - I will NOT fix this because random stuff will break
   templateUrl: './techd-table-row.component.html',
   styleUrls: ['./techd-table-row.component.css']
 })
 export class TechdTableRowComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+
+  }
 
   ngOnInit(): void {
   }
 
-  @Input() fields!: Array<String>;
-  @Input() header: Boolean = false;
-  @Input() Completed : Boolean = false;
+  @Input() rows: Array<TableRow> = [
+    new TableRow(['A', 'B', 'C', 'D'], false),
+    new TableRow(['A', 'B', 'C', 'D'], true),
+  ];;
+
+}
+
+export interface TableRowInt {
+  Fields: Array<String>;
+  Completed: Boolean;
+}
+
+export class TableRow implements TableRowInt //one row for the entire table
+{
+  Fields: Array<String>;
+  Completed: Boolean;
+
+  constructor(fields: Array<string>, completed: boolean)
+  {
+    this.Fields = fields;
+    this.Completed = completed;
+  }
 }
