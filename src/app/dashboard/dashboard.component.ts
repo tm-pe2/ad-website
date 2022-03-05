31ab5eea05e57a938574ff6544ffccd0a90c47c5
DrunkenToast <peter5leconte@gmail.com>
2022-03-05 19:47:53 +0100

    Add dummy API

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,44 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser'
 import { ActivatedRoute } from '@angular/router';
 
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  dummyUsers: User[] = [
-    {
-      id: 0,
-      name: "Bob Johnson",
-      email: "example@example.example",
-      phone: "+01 23/45/67/89",
-      address: "59 Golden Leaf Road",
-      birthday: "19/05/1965",
-      pfpsrc: "../../assets/img/dashboard/human.webp",
-      type: "Employee"
-    },
-    {
-      id: 1,
-      name: "Jeef Cumbersome",
-      email: "example@example.example",
-      phone: "+01 23/45/67/89",
-      address: "3155 Carioca Hill",
-      birthday: "23/06/1980",
-      pfpsrc: "../../assets/img/dashboard/tech.webp",
-      type: "Technician"
-    },
-    {
-      id: 2,
-      name: "Abigail Janssens",
-      email: "example@example.example",
-      phone: "+01 23/45/67/89",
-      address: "0287 Mayer Hill",
-      birthday: "23/03/1995",
-      pfpsrc: "../../assets/img/dashboard/woman.jpg",
-      type: "User"
-    },
-  ]
   user?: User
   title: string = 'Dashboard'
 
@@ -47,10 +16,20 @@ export class DashboardComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.titleService.setTitle('Dashboard')
+    this.titleService.setTitle('Dashboard');
+
     // For dummy testing
-    this.user = this.dummyUsers[Number(this.activatedRoute.snapshot.paramMap.getAll("userType"))]
-    this.title = 'Dashboard ' + this.user.name
+    fetch('http://localhost:6060/dashboard/user/' + Number(this.activatedRoute.snapshot.paramMap.getAll("userType")))
+      .then((res) => res.json())
+      .then((user: User) => {
+        console.debug(user);
+        this.user = user;
+        this.title = `Welcome, ${this.user.name}`
+      })
+      .catch((err) => {
+        this.title = 'Error retrieving user'
+        console.error('Error retrieving user', err);
+      });
   }
 }
 
