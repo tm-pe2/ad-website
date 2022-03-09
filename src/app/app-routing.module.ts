72c3db5b99e7a395cf766638bc2db0bff8b7dbb0
RubenSle <96013479+RubenSle@users.noreply.github.com>
2022-03-09 14:47:53 +0100

    Integrate Dummy data in ts file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ManageinvoicesComponent } from './manageinvoices/manageinvoices.component';
 import { TicketingComponent } from './ticketing/ticketing.component';
 
 const routes: Routes = [
   {path: 'dashboard', component: DashboardComponent},
-  {path: 'ticketing', component: TicketingComponent}
+  {path: 'ticketing', component: TicketingComponent},
+  {path: 'manageInvoices', component: ManageinvoicesComponent}
 ];
 
 @NgModule({
--- src/app/manageinvoices/manageinvoices.component.ts
+++ src/app/manageinvoices/manageinvoices.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-manageinvoices',
@@ -6,21 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manageinvoices.component.css']
 })
 export class ManageinvoicesComponent implements OnInit {
+
+  public bills = 
+  [{"id":1,"ClientID":1,"SupplierID":1,"Date":"3/23/2021","Type":"Gas","Amount":820,"Price":577,"Tax":13},
+  {"id":2,"ClientID":2,"SupplierID":2,"Date":"3/18/2021","Type":"Electricity","Amount":685,"Price":444,"Tax":13},
+  {"id":3,"ClientID":1,"SupplierID":2,"Date":"12/6/2021","Type":"Electricity","Amount":763,"Price":167,"Tax":5}];
+  public people = 
+  [{"ClientID":1,"FirstName":"Chrisse","LastName":"Sulter","email":"csulter0@imageshack.us"},
+  {"ClientID":2,"FirstName":"Boone","LastName":"Bodle","email":"bbodle1@umich.edu"},
+  {"ClientID":3,"FirstName":"Wilie","LastName":"Renison","email":"wrenison2@last.fm"}];
+  public contracts = 
+  [{"ContractID":1,"ClientID":1,"ClientType":"Company"},
+  {"ContractID":2,"ClientID":1,"ClientType":"Individual"},
+  {"ContractID":3,"ClientID":2,"ClientType":"Individual"}];
   
-  people = [
-    {"ID": 1, "FirstName": "John", "LastName": "Doe", "bills": [
-      {"ID": 3, "Date": "24/02/2022", "Type": "Gas", "Supplier": "Esso", "Amount": 500, "Charge": 100},
-      {"ID": 10, "Date": "03/03/2022", "Type": "Electricity", "Supplier": "Electrabel", "Amount": 4500, "Charge": 50}
-    ]},
-    {"ID": 2, "FirstName": "Amy", "LastName": "Spiller", "bills": [
-
-    ]}]
-
-
-
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
