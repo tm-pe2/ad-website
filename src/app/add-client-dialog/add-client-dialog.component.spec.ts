f37434cafcd079562a3a40c640050d89a753b139
klararapo <91880262+klararapo@users.noreply.github.com>
2022-03-10 15:06:57 +0100

    Commented out the default test files

--- src/app/add-client-dialog/add-client-dialog.component.spec.ts
+++ src/app/add-client-dialog/add-client-dialog.component.spec.ts
@@ -1,25 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AddClientDialogComponent } from './add-client-dialog.component';
+// import { AddClientDialogComponent } from './add-client-dialog.component';
 
-describe('AddClientDialogComponent', () => {
-  let component: AddClientDialogComponent;
-  let fixture: ComponentFixture<AddClientDialogComponent>;
+// describe('AddClientDialogComponent', () => {
+//   let component: AddClientDialogComponent;
+//   let fixture: ComponentFixture<AddClientDialogComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AddClientDialogComponent ]
-    })
-    .compileComponents();
-  });
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       declarations: [ AddClientDialogComponent ]
+//     })
+//     .compileComponents();
+//   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AddClientDialogComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(AddClientDialogComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
