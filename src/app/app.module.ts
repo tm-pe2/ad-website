58d7a8a27258a147c5cdf6f497e85a27150d8e77
Coockie1173 <rudjerud@hotmail.com>
2022-03-17 11:34:21 +0100

    added API connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TechnicianTodoComponent } from './technician-todo/technician-todo.component';
 import { TechdTableRowComponent } from './technician-todo/techd-table-row/techd-table-row.component';
-import { TechFilterButtonsComponent } from './tech-filter-buttons/tech-filter-buttons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TechnicianTodoComponent,
     TechdTableRowComponent,
-    TechFilterButtonsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/technician-todo/techd-table-row/techd-table-row.component.ts
+++ src/app/technician-todo/techd-table-row/techd-table-row.component.ts
@@ -14,82 +14,32 @@ export class TechdTableRowComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.QueriedShow(new FilterParams());
+    fetch('http://localhost:6060/techtasks')
+    .then((response) => response.json())
+    .then((tasks) => {
+      console.table(tasks.Tasks);
+      this.rows = tasks.Tasks;
+    });
   }
 
-  @Input() rows: Array<TableRow> = [
-    new TableRow(['A', 'B', 'C', 'D'], false),
-    new TableRow(['A', 'C', 'C', 'D'], true),
-    new TableRow(['F', 'B', 'C', 'D'], false),
-    new TableRow(['F', 'C', 'C', 'D'], true),
-    new TableRow(['G', 'B', 'C', 'D'], false),
-    new TableRow(['G', 'C', 'C', 'D'], true),
-    new TableRow(['H', 'B', 'C', 'D'], false),
-    new TableRow(['H', 'C', 'C', 'D'], true),
+  @Input() rows: Array<TechTasks> = [
+
   ];;
 
-  @Input() QueryState: number = 0x01
-
-  @Input() LinesToShow: Array<TableRow> = new Array<TableRow>();
-
-  QueriedShow(FP: FilterParams)
+  RefreshData()
   {
-    /*let LinesToShow: Array<TableRow>;
-    LinesToShow = new Array<TableRow>();*/
-    /*this.LinesToShow = new Array<TableRow>();
-    if(this.QueryState == 0x01)
-    {
-      this.LinesToShow = this.rows;
-      return;
-    }
-    if(this.QueryState == 0x02)
-    {
-      this.rows.forEach((value) =>
-      {
-        if(!value.Completed)
-        {
-          this.LinesToShow.push(value);
-        }
-      })
-    }*/
-
-    this.LinesToShow = Object.assign([], this.rows);
-    if(FP.CompletedOnly)
-    {
-      let pos = 0;
-      let buf = new Array<TableRow>();
-      this.LinesToShow.forEach((value) =>
-      {
-        if(value.Completed)
-        {
-          buf.push(value);
-        }
-      })
-      this.LinesToShow = Object.assign([], buf);
-    }
-
-    return;
-  }
-
-  UpdateTableItem(ID: number, CheckedMode: boolean)
-  {
-    this.LinesToShow[ID].Completed = CheckedMode;
+    fetch('http://localhost:6060/techtasks')
+    .then((response) => response.json())
+    .then((tasks) => {
+      console.table(tasks.Tasks);
+      this.rows = tasks.Tasks;
+    });
   }
 }
 
-export interface TableRowInt {
-  Fields: Array<String>;
-  Completed: Boolean;
-}
-
-export class TableRow implements TableRowInt //one row for the entire table
-{
-  Fields: Array<String>;
-  Completed: Boolean;
-
-  constructor(fields: Array<string>, completed: boolean)
-  {
-    this.Fields = fields;
-    this.Completed = completed;
-  }
+interface TechTasks{
+  Name: String;
+  Location: String;
+  WhenDate: String;
+  Completed: boolean;
 }
