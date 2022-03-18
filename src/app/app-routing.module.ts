bd53bf13d767a05829d1c3f3f5bd16a3eb783894
Azety <tom.devyver@gmail.com>
2022-03-18 13:32:32 +0100

    fixie

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
 
 const routes: Routes = [
-  {path: 'header', component: HeaderComponent}
 ];
 
 @NgModule({
