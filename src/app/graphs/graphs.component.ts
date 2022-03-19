fcb88a038b0e3d24a4151d54210acebd481fe4f0
pauliendoms <paulien.doms@student.thomasmore.be>
2022-03-19 16:25:11 +0100

    basic example from api used in page

--- src/app/graphs/graphs.component.ts
+++ src/app/graphs/graphs.component.ts
@@ -8,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
 
 export class GraphsComponent implements OnInit {
 
+  usage?: Usage;
+  txt: String ='';
+
   constructor() {
   }
 
   ngOnInit(): void {
+    fetch('http://localhost:6060/energyUsage/0')
+      .then((res) => res.json())
+      .then((usage: Usage) => {
+        this.usage = usage;
+        this.txt = `Welcome, ${this.usage.client}, ${this.usage.usage}`
+      })
+      .catch((err) => {
+        this.txt = 'Error retrieving user'
+        console.error('Error retrieving user', err);
+      });
   }
   
+}
+
+export interface Usage {
+  id: Number,
+  client: String,
+  usage: Number,
 }
