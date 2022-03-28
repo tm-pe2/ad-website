6b79f33cb3641fd427f9f6e4b65246e7d0371e19
xanderaerts <xanderaerts@gmail.com>
2022-03-28 16:55:48 +0200

    style fix

--- src/app/manage-employees/edit-employee-form/edit-employee-form.component.ts
+++ src/app/manage-employees/edit-employee-form/edit-employee-form.component.ts
@@ -13,6 +13,17 @@ export class EditEmployeeFormComponent {
   //DepartmentOptions = ['mangers','idk','the guys from it'];
   
   emp = new Employee(1,"Jos","Van den Berg","1/1/1","josvandenberg@bedrijf.com","0412312312","thuis","1/1/2","managers","X"); 
+
+  FirstName = "";
+  LastName = "";
+  BirthDate= "";
+  Email = "";
+  PhoneNumber = "";
+  Address ="";
+  HireDate ="";
+  Department = "";
+  Gender="";
+
   
   submitted = false;
   
