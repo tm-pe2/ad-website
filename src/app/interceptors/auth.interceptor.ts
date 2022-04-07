7b1e75d97265e2aa93f75eb8daccdd43ed592c76
DrunkenToast <peter5leconte@gmail.com>
2022-04-07 13:05:20 +0200

    Blacklist more routes

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -10,7 +10,7 @@ import { Observable, tap } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 import { environment } from '../../environments/environment';
 
-const BLACKLIST = ['/login'];
+const BLACKLIST = ['/login', '/register', '/logout']; // TODO create logout route
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
