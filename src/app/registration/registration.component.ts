0ea49388aa83e3844221a3673d03667c37ae9f32
Sean De Decker <74727305+sdd2001@users.noreply.github.com>
2022-04-13 16:24:55 +0200

    regsitrationData
    added a registrationdata interface

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { RegistrationData } from '../interfaces/registrationData';
 
 @Component({
   selector: 'app-registration',
@@ -22,6 +23,22 @@ export class RegistrationComponent implements OnInit {
   // Public
   onSubmit(regForm: NgForm) 
   {
+    // use registerdata 
+    const regData: RegistrationData =
+    {
+      name: regForm.value.regName,
+      mail: regForm.value.regMail,
+      password: regForm.value.regPassword,
+      confirmPassword: regForm.value.regConfPassword,
+      phone: regForm.value.regPhone,
+      type: regForm.value.regType,
+      city: regForm.value.regCity,
+      zipcode: regForm.value.regPostalcode,
+      street: regForm.value.regStreet,
+      house: regForm.value.regNumber
+
+    }
+
     // Check if all the fields are filled in
     // Set invalidForm to true so the error message displays
     // Set matchingPasswords to false so the error message doesn't display
@@ -36,7 +53,7 @@ export class RegistrationComponent implements OnInit {
     // Check if the passwords match
     // Set matchingPasswords to true so the error message displays
     // Set invalidForm to false so the error message doesn't display
-    if (!(regForm.value.regPassword == regForm.value.regConfPassword))
+    if (!(regData.password == regData.confirmPassword))
     {
       this.invalidForm = false;
       this.matchingPasswords = true;
@@ -46,7 +63,7 @@ export class RegistrationComponent implements OnInit {
 
     // Print out the data to the console for demo
     // Delete this later when you can send it to the API
-    console.log(regForm.value);
+    console.log(regData);
 
   }
 
