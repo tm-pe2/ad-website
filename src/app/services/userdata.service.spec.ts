9257bd3da9c5aa8e25531bb6773c597c6533961d
Sean De Decker <74727305+sdd2001@users.noreply.github.com>
2022-04-18 15:02:09 +0200

    test errors
    Tried to get rid of the errors by putting the httpclient in the import in the service.specs.ts file.

--- src/app/services/userdata.service.spec.ts
+++ src/app/services/userdata.service.spec.ts
@@ -1,15 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
 import { UserdataService } from './userdata.service';
 import { RouterTestingModule } from '@angular/router/testing';
 import { FormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('UserdataService', () => {
   let service: UserdataService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule, FormsModule]
+      imports: [RouterTestingModule, FormsModule, HttpClientTestingModule]
     });
     service = TestBed.inject(UserdataService);
   });
