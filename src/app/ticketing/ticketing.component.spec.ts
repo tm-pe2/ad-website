216b08b4e9caa46ff889b71d544bb37f0462ceab
DrunkenToast <peter5leconte@gmail.com>
2022-05-11 13:15:11 +0200

    import fix

--- src/app/ticketing/ticketing.component.spec.ts
+++ src/app/ticketing/ticketing.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 
-import { TicketingComponent, TableRow } from './ticketing.component';
+import { TicketingComponent } from './ticketing.component';
 
 describe('TicketingComponent', () => {
   let component: TicketingComponent;
