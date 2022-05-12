c5e26f5d2c36505de793d8806d25feacff11c2b5
Azety <tom.devyver@gmail.com>
2022-05-12 09:54:46 +0200

    fex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UtilService } from './util.service';
-
-import { DatePipe } from '@angular/common';
 import { HeaderComponent } from './workerapp/header/header.component';
 import { WorkerappComponent } from './workerapp/workerapp.component';
+import { LoginPageComponent } from './workerapp/login-page/login-page.component';
 import { CientListComponent } from './workerapp/cient-list/cient-list.component';
-import { ListElementComponent } from './workerapp/cient-list/list-element/list-element.component';
 import { CientPageComponent } from './workerapp/cient-page/cient-page.component';
+import { ListElementComponent } from './workerapp/cient-list/list-element/list-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     WorkerappComponent,
+    LoginPageComponent,
     CientListComponent,
-    ListElementComponent,
-    CientPageComponent
+    CientPageComponent,
+    ListElementComponent
   ],
   entryComponents: [],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [UtilService,DatePipe],
+  providers: [
+    UtilService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
