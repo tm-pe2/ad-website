ff9fd2132bdba49a81d80e6b4524d72389660c27
Azety <tom.devyver@gmail.com>
2022-05-17 15:03:00 +0200

    This reverts commit 2650d0d369252407f9b87e6472dd0b41e4bdfe6a.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,10 +28,13 @@ import { StatuscodepageComponent } from './statuscodepage/statuscodepage.compone
 import { SupportComponent } from './support/support.component';
 import { WorkerappComponent } from './workerapp/workerapp.component';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { ListElementComponent } from './workerapp/cient-list/list-element/list-element.component';
 import { CientListComponent } from './workerapp/cient-list/cient-list.component';
 import { CientPageComponent } from './workerapp/cient-page/cient-page.component';
 import { HeaderComponent } from './workerapp/header/header.component';
+=======
+>>>>>>> parent of d0a21fc (nog altij kpot)
 =======
 >>>>>>> parent of d0a21fc (nog altij kpot)
 
@@ -57,10 +60,13 @@ import { HeaderComponent } from './workerapp/header/header.component';
     ManageTickets,
     WorkerappComponent,
 <<<<<<< HEAD
+<<<<<<< HEAD
     ListElementComponent,
     CientListComponent,
     HeaderComponent,
     CientPageComponent,
+=======
+>>>>>>> parent of d0a21fc (nog altij kpot)
 =======
 >>>>>>> parent of d0a21fc (nog altij kpot)
   ],
--- src/app/workerapp/workerapp.component.ts
+++ src/app/workerapp/workerapp.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UtilService } from '../util.service';
 import {} from '@angular/core';
+
+
 @Component({
   selector: 'app-workerapp',
   templateUrl: './workerapp.component.html',
