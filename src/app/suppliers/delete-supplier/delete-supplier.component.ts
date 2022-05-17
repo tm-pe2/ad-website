8f96de801ab4d65e789676aa92e45c03e9379007
KilliBruhh <61047380+KilliBruhh@users.noreply.github.com>
2022-05-17 11:30:24 +0200

    delete style

--- src/app/suppliers/delete-supplier/delete-supplier.component.ts
+++ src/app/suppliers/delete-supplier/delete-supplier.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 
@@ -15,19 +16,18 @@ export class DeleteSupplierComponent implements OnInit {
   }
 
  
-  gotToPage(pageName:string):void{
+  backToHub(pageName:string):void{
     this.router.navigate([`${pageName}`]);
   }
 
+  presDeleted = false;
   
 /*   once you pressed the button there will be an alert
      telling you the supplier has been deleted and automaticly 
      redirect you back to the main hub
 */
-  supplierPage = "suppliers"
-  deleteSupplier(supName:string):void{
-    alert(supName + " has been Deleted")
-    this.router.navigate([`${this.supplierPage}`]);
+  onSubmit(deleteSupplier: NgForm){
+    this.presDeleted = true;
   }
 
 }
