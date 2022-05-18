b65bf39c723fab9c0d2e0188293ad79c3fe7c744
Dries1234 <driesolbrechts@gmail.com>
2022-05-18 22:55:32 +0200

    merge¨: git commit -m merge

--- src/app/interfaces/address.ts
+++ src/app/interfaces/address.ts
@@ -1,17 +1,8 @@
 export interface Address{
-<<<<<<< HEAD
-        adressID: number,
-        city: string,
-        street: string,
-        postalcode: number,
-        housNumber: number,
-        country: string
-=======
     adressID: number,
     city: string,
     street: string,
     postalcode: number,
     housNumber: number,
     country: string
->>>>>>> main
 }
