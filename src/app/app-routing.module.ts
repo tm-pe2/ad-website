7573e5fb367e6b4e9ba808b138239547fcce234d
Dries1234 <driesolbrechts@gmail.com>
2022-05-18 21:07:33 +0200

    pls

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+import { AddSuplierComponent } from './suppliers/add-suplier/add-suplier.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CustomerComponent } from './customers/customers.component';
 import { TicketingComponent } from './ticketing/ticketing.component';
@@ -13,8 +15,17 @@ import { RoleGuardService } from './services/role-guard.service';
 import { LogoutComponent } from './logout/logout.component';
 import { StatuscodepageComponent } from './statuscodepage/statuscodepage.component';
 import { SupportComponent } from './support/support.component';
+import { UserRole } from './interfaces/User';
+import { EditSupplierComponent } from './suppliers/edit-supplier/edit-supplier.component';
+import { DeleteSupplierComponent } from './suppliers/delete-supplier/delete-supplier.component';
+import { DetailsSupplierComponent } from './suppliers/details-supplier/details-supplier.component';
+import { ContactSupplierComponent } from './suppliers/contact-supplier/contact-supplier.component';
+import { ContractOverveiwComponent } from './suppliers/contract-overveiw/contract-overveiw.component';
+import { ManageContractComponent } from './suppliers/manage-contract/manage-contract.component';
+import { SuppliersComponent } from './suppliers/suppliers.component';
 
 const routes: Routes = [
+  {path: "", redirectTo: "dashboard", pathMatch: "full"},
   {
     path: 'dashboard',
     component: DashboardComponent,
@@ -24,21 +35,30 @@ const routes: Routes = [
     path: 'ticketing',
     component: TicketingComponent,
     canActivate: [RoleGuardService],
-    data: { roles: ['client', 'admin'] }, // Only users with the role 'client' or 'admin' can access this route
+    data: { roles: [UserRole.ADMIN, UserRole.TECHNICIAN] }, // E.g. how to user roleguard service
   },
   {path: 'support', component: SupportComponent},
   {path: 'login', component: LoginComponent},
   {path: 'logout', component: LogoutComponent},
   {path: 'registration', component: RegistrationComponent},
   {path: 'ticketing/:userType', component: TicketingComponent},
-  {path: 'graphs', component: GraphsComponent},
+  {path: 'graphs', component: GraphsComponent, canActivate: [RoleGuardService], data: { roles: [UserRole.CUSTOMER] }},
   {path: 'statuscode/:statusCode', component: StatuscodepageComponent},
-  {path: 'dashboard', component: DashboardComponent},
-  {path: 'ticketing', component: TicketingComponent},
   {path:'manage-customers',component:CustomerComponent},
   {path:'consumption-estimation',component:ConsumptionEstimationComponent},
   {path:'register-customer',component:RegisterCustomerComponent},
+  {path: 'add-suplier', component: AddSuplierComponent, canActivate: [RoleGuardService], data: {roles: [UserRole.ADMIN, UserRole.MANAGER]}},
+  {path: 'edit-supplier', component: EditSupplierComponent, canActivate: [RoleGuardService], data: {roles: [UserRole.ADMIN, UserRole.MANAGER]}},
+  {path: 'delete-supplier', component: DeleteSupplierComponent, canActivate: [RoleGuardService], data: {roles: [UserRole.ADMIN, UserRole.MANAGER]}},
+  {path: 'details-supplier', component: DetailsSupplierComponent, canActivate: [RoleGuardService], data: {roles: [UserRole.ADMIN, UserRole.MANAGER]}},
+  {path: 'contact-supplier', component: ContactSupplierComponent, canActivate: [RoleGuardService], data: {roles: [UserRole.ADMIN, UserRole.MANAGER]}},
+  {path: 'contract-overview', component: ContractOverveiwComponent, canActivate: [RoleGuardService], data: {roles: [UserRole.ADMIN, UserRole.MANAGER]}},
+  {path: 'manage-contract', component: ManageContractComponent, canActivate: [RoleGuardService], data: {roles: [UserRole.ADMIN, UserRole.MANAGER]}},
+  {path: 'suppliers', component: SuppliersComponent, canActivate: [RoleGuardService], data: {roles: [UserRole.ADMIN, UserRole.MANAGER]}},
 ]
+
+
+
 
 @NgModule(
   {
