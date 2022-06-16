cad71da95d8e73040f944a5baba9f3cd9c11231d
Dries1234 <driesolbrechts@gmail.com>
2022-06-16 12:37:47 +0200

    type -> customer_type

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -107,7 +107,7 @@ export class FormsComponent implements OnInit {
           house_number: this.form.get('house_number')?.value,
           city_id: this.form.get('city')?.value as number
         }],
-        customer_type: this.form.get('type')?.value as CustomerType,
+        customer_type: this.form.get('customer_type')?.value as CustomerType,
         password: this.form.get('password')?.value,
       };
       for (let field of this.registerFields) {
