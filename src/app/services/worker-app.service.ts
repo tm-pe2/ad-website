a091493c9d436a71d85782042d1490af85fbba3f
Sean De Decker <74727305+sdd2001@users.noreply.github.com>
2022-06-17 17:12:24 +0200

    Revert "123"
    This reverts commit 9a1ed56c1c661e167b0ebb55e9a6adf81c454208.

--- src/app/services/worker-app.service.ts
+++ src/app/services/worker-app.service.ts
@@ -17,6 +17,8 @@ export class WorkerAppService {
   // Variables
   planningItem?: Planning;
   planningList: Array<Planning> = [];
+  userIDs: Array<number> = [];
+  consumption: Array<Consumption> = [];
   meters: Array<Meter> = [];
 
   selectedUser : number = 0;
@@ -40,8 +42,7 @@ export class WorkerAppService {
               if (plan.status == 1){ 
                 //check if this entry is already in the list or not
                 for (let i = 0; i < this.planningList.length; i++) {
-                  if(plan.id == this.planningList[i].id
-                      && plan.date == this.planningList[i].date){
+                  if(plan.user.id == this.planningList[i].user.id){
                     unique = false;
                   }
                 }
@@ -50,6 +51,7 @@ export class WorkerAppService {
                 {
                   console.log(plan); 
                   this.planningList.push(plan); 
+
                 } 
                 
               }
@@ -74,20 +76,37 @@ export class WorkerAppService {
   {
     return new Promise<void>((resolve, reject) => {
       this.http.post(environment.apiUrl + '/consumptions/', meters, {responseType: 'text'}).subscribe({
-          next: (res: any) => { console.log(res); resolve(res); },
+          next: (res: any) =>
+            { 
+              console.log('post');
+              this.patchPlanning();
+              resolve(res);
+              
+            },
           error: (err) => { reject(err); console.log('NAY'); }
         });
     });
 
   }
 
-  public patchPlanning(id: number)
+  public patchPlanning()
   {
     return new Promise<void>((resolve, reject) => 
     {
-      this.http.patch(environment.apiUrl + '/plannings/' + id, {status: PlanningStatus.DONE}, {responseType: 'text'}).subscribe(
+      this.http.patch(environment.apiUrl + '/plannings/' + this.planningID, {status: PlanningStatus.DONE}, {responseType: 'text'}).subscribe(
         {
-          next: (res: any) => { console.log(res); },
+          next: (res: any) =>
+            { 
+              for (let i = 0; i < this.planningList.length; i++)
+              {
+                if (this.planningList[i].id == this.planningID)
+                { this.planningList.splice(i, 1); }
+
+              }
+              console.log('patch');
+              resolve(res);
+            
+            },
           error: (err) => { reject(err); }
           
         });
